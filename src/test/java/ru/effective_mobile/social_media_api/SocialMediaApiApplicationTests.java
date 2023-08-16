9c1da2789711cbb9bedcaabbfb460ae2517390c3
Eldar <ElMAkhmadeev@Greenatom.ru>
2023-08-16 15:45:58 +0300

    Initial commit

---
+++ src/test/java/ru/effective_mobile/social_media_api/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effective_mobile.social_media_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
